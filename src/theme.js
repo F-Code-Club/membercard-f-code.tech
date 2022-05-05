b36a0a9d0f5fef07430ca94c975dbbc04b65d0f0
Huynh Hoang Huy <akirahuynh2002@gmail.com>
2022-05-05 22:17:20 +0700

    Update global color and add global CSS rules

--- src/theme.js
+++ src/theme.js
@@ -1,17 +1,21 @@
 // colors
 const theme = {
-  high_contrast: "#11181C",
-  state1: "#687076",
-  state2: "#F8F9FA",
-  teal: "#067A6F",
-  cyan1: "#00B1C4",
-  cyan2: "#A9DDE5",
-  indigo1: "#E6EDFE",
-  indigo2: "#E6EDFE",
-  indigo3: "#3451B2",
-  plum: "#FEFCFF",
-  red1: "#CD2B31",
-  red2: "#FFE5E5",
+  high_contrast: '#11181C',
+  low_contrast: '#54585C',
+  state1: '#687076',
+  state2: '#F8F9FA',
+  slate3: '#EFEEF1',
+  slate4: '#C4C4C4',
+  teal: '#067A6F',
+  cyan1: '#00B1C4',
+  cyan2: '#A9DDE5',
+  indigo1: '#E6EDFE',
+  indigo2: '#E6EDFE',
+  indigo3: '#3451B2',
+  plum: '#FEFCFF',
+  red1: '#CD2B31',
+  red2: '#FFE5E5',
+  red3: '#F5C0C1',
 };
 
 export default theme;
