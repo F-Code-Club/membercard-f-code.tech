582413e3f3f0f989e073ecf9471d3235a65ad40a
Huynh Hoang Huy <akirahuynh2002@gmail.com>
2022-05-09 10:40:48 +0700

    Refactor Icon component

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,21 +1,23 @@
 import styled from 'styled-components';
 
-const Icon = ({ className, name }) => {
-  return (
-    <div className={className}>
-      <ion-icon name={name}></ion-icon>
-    </div>
-  );
-};
-
-const StyledIcon = styled(Icon)`
+const StyledIcon = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: ${(props) => props.size || 'inherit'};
   & > ion-icon {
     font-size: inherit;
+    --ionicon-stroke-width: ${props => props.weight || '16px'}em;
   }
 `;
 
-export default StyledIcon;
+const Icon = (props) => {
+  const { children, name, ...rest } = props;
+  return (
+    <StyledIcon {...rest}>
+      <ion-icon name={name}></ion-icon>
+    </StyledIcon>
+  );
+};
+
+export default Icon;
