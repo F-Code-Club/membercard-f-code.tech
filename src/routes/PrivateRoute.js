b7e868684f694c97be6d43db7b17bd31152b0ee0
Hiep Hoang <cnhh85@gmail.com>
2022-06-05 23:45:28 +0700

    try to get api from be

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 import { Navigate, Outlet } from 'react-router-dom'
 
+import { get } from '../utils/ApiCaller'
 import LocalStorageUtils from '../utils/LocalStorageUtils'
 
 export const PrivateRoute = () => {
   const user = LocalStorageUtils.getUser()
+  if (user && user.id?.length >= 0) {
+    get('/api/user/' + user.id, {}, { token: LocalStorageUtils.getToken() })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err))
+  }
   return user && user.id?.length >= 0 ? <Outlet /> : <Navigate to="/login" replace />
 }
