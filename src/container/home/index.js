9b1a3c690e682fccfa6490bd18fad9f19b319680
Hiep Hoang <cnhh85@gmail.com>
2022-06-07 13:06:15 +0700

    add user get handle

--- src/container/home/index.js
+++ src/container/home/index.js
@@ -1,7 +1,34 @@
+import { useState, useEffect } from 'react'
+
+import { Navigate } from 'react-router-dom'
+
+import { get } from '../../utils/ApiCaller'
+import LocalStorageUtils from './../../utils/LocalStorageUtils'
 import { Content } from './style'
 
 const Home = () => {
-  return <Content>Welcome home</Content>
+  const [data, setData] = useState([])
+  const getData = async () => {
+    const user = LocalStorageUtils.getUser()
+    const token = LocalStorageUtils.getToken()
+    let response
+    await get('/api/user/' + user.id, {}, { token: token })
+      .then((res) => (response = res))
+      .catch((err) => {
+        LocalStorageUtils.removeItem('token')
+        return <Navigate to="/login" replace />
+      })
+    setData(response)
+  }
+  useEffect(() => {
+    getData()
+  }, [])
+  console.log(data)
+  if (data.data?.status === 403) {
+    LocalStorageUtils.removeItem('token')
+    return <Navigate to="/login" replace />
+  }
+  return <Content>Hello</Content>
 }
 
 export default Home
