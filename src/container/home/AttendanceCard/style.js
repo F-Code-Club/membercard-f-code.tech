dc6404fa6055e2dccecbb7b08bd413646cff3af9
Nghĩa <nghia14302@gmail.com>
2022-06-19 10:59:55 +0700

    FIX -Scale card logo and font smaller

--- src/container/home/AttendanceCard/style.js
+++ src/container/home/AttendanceCard/style.js
@@ -3,8 +3,8 @@ import styled from 'styled-components'
 import theme from './../../../theme'
 
 export const StyledImage = styled.img`
-  width: 160px;
-  height: 218px;
+  width: 42%;
+  height: 50%;
   object-fit: contain;
   flex-grow: 1;
   background-color: ${theme.state4};
@@ -12,7 +12,7 @@ export const StyledImage = styled.img`
 
 export const StyledCardTitle = styled.p`
   font-family: 'Inter', sans-serif;
-  font-size: 40px;
+  font-size: 2.1em;
   font-weight: 700;
   color: ${theme.low_contrast};
   line-height: 48px;
