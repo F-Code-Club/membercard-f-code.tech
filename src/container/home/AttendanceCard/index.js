4772ec6ecefc93a32c5f6d4edc5dd1e70da9b2a5
Nghĩa <nghia14302@gmail.com>
2022-06-21 19:35:43 +0700

    ADD - create card reader future

--- src/container/home/AttendanceCard/index.js
+++ src/container/home/AttendanceCard/index.js
@@ -1,17 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Button } from '../../../components/Button'
 import Divider from '../../../components/Divider'
 import Modal from '../../../components/Modal'
 import Wrapper from '../../../components/Wrapper'
 import Flexbox from './../../../components/Flexbox'
+import TextArea from './../../../components/Input/TextArea'
 
 import CardSvg from './../../../asset/image/Card.svg'
+// import { cardReader } from './../../../utils/CardReader'
 import { StyledCardTitle, StyledImage } from './style'
 
 const AttendanceCard = (props) => {
   const { data, onClose, openViewList } = props
+  const [cardReader, setCardReader] = useState({
+    log: '',
+    status: '',
+  })
+  // window.addEventListener('error', function (error) {
+  //   if (cardReader && cardReader.status) {
+  //     console.error(error)
+  //     cardReader.setStatus(error.message + ' (Your browser may not support this feature.)')
+  //     error.preventDefault()
+  //   }
+  // })
+  const onScan = async () => {
+    setCardReader({
+      log: 'Start Scanning',
+      status: null,
+    })
+    console.log('Scanning')
 
+    try {
+      if (!('NDEFReader' in window)) {
+        throw new Error('NDEFReader is not available, use Chrome on Android')
+      }
+      const ndef = new NDEFReader()
+      await ndef.scan()
+      ndef.addEventListener('readingerror', () => {
+        log('Argh! Cannot read data from the NFC tag. Try another one?')
+      })
+
+      ndef.addEventListener('reading', ({ message, serialNumber }) => {
+        setCardReader({
+          log: `> Serial Number: ${serialNumber} > Records: (${message.records.length})`,
+          status: '',
+        })
+      })
+    } catch (error) {
+      setCardReader({ log: 'Argh! ' + error, status: error.message })
+    }
+  }
+
+  useEffect(() => {
+    if (/Chrome\/(\d+\.\d+.\d+.\d+)/.test(navigator.userAgent)) {
+      // Let's log a warning if the sample is not supposed to execute on this
+      // version of Chrome.
+      if (89 > parseInt(RegExp.$1)) {
+        setCardReader({
+          log: '',
+          status: 'warning your browser my not supported',
+        })
+      }
+    }
+    onScan()
+  }, [])
   return (
     <Modal show={data.show} title="Check Attendance" onClose={onClose}>
       <Wrapper minHeight="450px">
@@ -23,11 +76,12 @@ const AttendanceCard = (props) => {
           justifyContent="center"
           alignItems="center"
         >
-          <StyledImage src={CardSvg} alt="put Member Card here" />
+          <StyledImage src={CardSvg} onClick={onScan} alt="put Member Card here" />
           <StyledCardTitle>Scan your card to continue</StyledCardTitle>
         </Flexbox>
       </Wrapper>
       <Divider variant="dashed" />
+      <TextArea value={cardReader.log} onChange={onScan}></TextArea>
       <Button onClick={openViewList}>View List</Button>
     </Modal>
   )
