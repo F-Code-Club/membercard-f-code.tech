087766630eac440d3087585ca4ecd680d153731d
List Kelvin <phamm5687@gmail.com>
2022-12-07 15:34:27 +0700

    Update SubModale butoon

--- src/components/Modal/SubModal/index.js
+++ src/components/Modal/SubModal/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import { GreenButton } from '../../../components/Button'
+
+import LocalStorageUtils from '../../../utils/LocalStorageUtils'
+import productApi from '../../../utils/productApi'
 // import { StyledEventIndicator } from '../../../container/home/style'
 import { CloseButton } from '../../Button'
 import {
@@ -13,6 +17,17 @@ import Wrapper from './../../Wrapper/index'
 import { SubHeading as SubTitle } from './style'
 
 const SubModal = (props) => {
+  const UpdateAttend = () => {
+    const token = LocalStorageUtils.getToken()
+    const userId = LocalStorageUtils.getJWTUser().id
+    console.log(userId)
+    productApi
+      .setAttendance(userId, 1653496208366, token)
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => console.log(err))
+  }
   if (!props.show) {
     return null
   }
@@ -26,6 +41,7 @@ const SubModal = (props) => {
             <SubTitle>{props.subTitle}</SubTitle>
           </StyledModalHeader>
           <StyledModalBody>{props.children}</StyledModalBody>
+          <GreenButton onClick={UpdateAttend}>Update</GreenButton>
         </StyledModalContent>
       </Wrapper>
     </StyledModal>
